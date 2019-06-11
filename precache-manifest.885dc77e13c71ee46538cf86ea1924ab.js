89c0a4980e746fb732ded1ea67efd1074953aad1
Vincent Humeau <hello@vincent-humeau.com>
2019-06-11 22:58:27 +0100

    Updates

---
+++ precache-manifest.885dc77e13c71ee46538cf86ea1924ab.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hackerNews/robots.txt"
+  },
+  {
+    "revision": "8faac4a636a7c1e8be0d",
+    "url": "/hackerNews/js/chunk-vendors.6bf03b79.js"
+  },
+  {
+    "revision": "ff7b67fcaafb98801242",
+    "url": "/hackerNews/js/app.63805238.js"
+  },
+  {
+    "revision": "30abc2594a62bbf719ecb3598994df59",
+    "url": "/hackerNews/index.html"
+  },
+  {
+    "revision": "ff7b67fcaafb98801242",
+    "url": "/hackerNews/css/app.a5f0b954.css"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/hackerNews/browserconfig.xml"
+  }
+];
